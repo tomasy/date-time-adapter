5812c5c45d5afae48de99d993883baa310c1e188
Michael Stegeman <michael@stegeman.me>
2020-04-13 08:55:42 -0800

    Make things a bit less chatty by default.
    Also, minor cleanups.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-"""DateTime addon for Mozilla IoT Gateway."""
+"""DateTime addon for Mozilla WebThings Gateway."""
 
 from os import path
 import functools
-import gateway_addon
 import logging
 import signal
 import sys
@@ -13,10 +12,8 @@ sys.path.append(path.join(path.dirname(path.abspath(__file__)), 'lib'))
 
 from pkg.date_adapter import DateTimeAdapter
 
-_API_VERSION = {
-    'min': 2,
-    'max': 2,
-}
+
+_DEBUG = False
 _ADAPTER = None
 
 print = functools.partial(print, flush=True)
@@ -38,18 +35,14 @@ if __name__ == '__main__':
         , stream = sys.stdout
     )
     logging.info('Starting DateTime Addon')
-    if gateway_addon.API_VERSION < _API_VERSION['min'] or \
-            gateway_addon.API_VERSION > _API_VERSION['max']:
-        logging.error('Unsupported API version. ver: %s', gateway_addon.API_VERSION)
-        sys.exit(0)
 
     try:
-        logging.info('Starting date-time-adapter. gateway_addon.API_VERSION: %s', gateway_addon.API_VERSION)
-        logging.debug('Arguments list: %s', str(sys.argv))
         signal.signal(signal.SIGINT, cleanup)
         signal.signal(signal.SIGTERM, cleanup)
-        _ADAPTER = DateTimeAdapter(verbose=True)
-        # Wait until proxy stops running. this indicats that the gateway has shut down.
+        _ADAPTER = DateTimeAdapter(verbose=_DEBUG)
+
+        # Wait until the proxy stops running, indicating that the gateway shut us
+        # down.
         while _ADAPTER.proxy_running():
             time.sleep(2)
     except Exception as ex:
