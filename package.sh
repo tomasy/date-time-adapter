f780e4ec1e5d2bd80da759746ef4dac773d94b24
William Fahle <billfahle@gmail.com>
2021-03-09 20:31:23 -0600

    Updated to cross compiler

--- package.sh
+++ package.sh
@@ -4,6 +4,13 @@ set -e
 
 version=$(grep version package.json | cut -d: -f2 | cut -d\" -f2)
 
+if [ -z "${ADDON_ARCH}" ]; then
+    TARFILE_SUFFIX=
+else
+    PYTHON_VERSION="$(python3 --version 2>&1 | cut -d' ' -f2 | cut -d. -f 1-2)"
+    TARFILE_SUFFIX="-${ADDON_ARCH}-v${PYTHON_VERSION}"
+fi
+
 # Clean up from previous releases
 rm -rf *.tgz package
 rm -f SHA256SUMS
@@ -14,19 +21,23 @@ mkdir lib
 mkdir package
 
 # Pull down Python dependencies
-pip3 install -r requirements.txt -t lib --no-binary pyHS100 --prefix ""
+pip3 install -r requirements.txt -t lib --no-binary :all: --prefix ""
 
 # Put package together
 #cp -r lib pkg LICENSE README.md package.json *.py package/
-cp -r pkg LICENSE README.md package.json manifest.json *.py requirements.txt setup.cfg package/
+cp -r lib pkg LICENSE README.md package.json manifest.json *.py package/
 find package -type f -name '*.pyc' -delete
 find package -type d -empty -delete
 
 # Generate checksums
 cd package
-sha256sum *.py pkg/*.py requirements.txt setup.cfg *.json README.md LICENSE > SHA256SUMS
+sha256sum *.py pkg/*.py *.json README.md LICENSE > SHA256SUMS
 #find lib -type f -exec sha256sum {} \; >> SHA256SUMS
 cd -
 
 # Make the tarball
-tar czf "date-time-adapter-${version}.tgz" package
+TARFILE="date-time-adapter-${version}${TARFILE_SUFFIX}.tgz"
+tar czf ${TARFILE} package
+
+shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+
